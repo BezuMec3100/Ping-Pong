c8144982c9135cd05d89aba482a8c50845314def
BezuMec3100 <bezumec3100@gmail.com>
2023-12-12 16:59:45 +0300

    изминение

--- game.py
+++ game.py
@@ -23,23 +23,36 @@ class Gamesprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(Gamesprite):
-    def update(self):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__(player_image, player_x, player_y, player_speed)
+        self.image = transform.scale(image.load (player_image), (30, 100))
+    def update1(self):
         keys = key.get_pressed()
-        if keys[K_a] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_d] and self.rect.x < 625:
-            self.rect.x += self.speed
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 450:
+            self.rect.y += self.speed
+    def update2(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 450:
+            self.rect.y += self.speed
 
-player1 = Player("redrak.png", 0, 300, 5)
-player2 = Player("bluerak.png", 700, 300, 5)
+player1 = Player("red2.png", 0, 300, 5)
+player2 = Player("blue2.png", 670, 300, 5)
 ball1 = Gamesprite("ball.png", 400, 300, 5)
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
+    player1.update1()
+    player2.update2()
     player1.reset()
     player2.reset()
     ball1.reset()
 
-    clock.tick(FPS)
+
     display.update()
+    clock.tick(FPS)
+
